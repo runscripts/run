85db023f18e66cd6535f1e2978b8f124cf131e83
tobe <tobeg3oogle@gmail.com>
2014-11-09 20:42:44 +0800

    Implement to git clone and execute script

--- run.go
+++ run.go
@@ -44,31 +44,73 @@ func main() {
   log.Warn(*updateFlag)
 
   // Create $HOME/runscript/ if it doesn't exist
-  runscriptDirectory := os.Getenv("HOME") + "/runscript/"
+  runscriptDirectory := os.Getenv("HOME") + "/runscript"
   utils.MkdirIfNotExist(runscriptDirectory)
 
   // Create $HOME/runscript/official/ if it doesn't exist
-  officialDirectory := os.Getenv("HOME") + "/runscript/official/"
+  officialDirectory := os.Getenv("HOME") + "/runscript/official"
   utils.MkdirIfNotExist(officialDirectory)
 
   // Check if it's official scripts
   scriptArray := strings.Split(args[1], "/")
   if len(scriptArray) == 1 {
-    log.Error("It's official")
+    // check if $HOME/runscript/official/script exists
+    repositoryDirectory := runscriptDirectory + "/official/script"
+    scriptName := scriptArray[0]
+    scriptPath := repositoryDirectory + "/" + scriptName
+
+    if (utils.IsDirectoryExist(repositoryDirectory)) {
+      // Todo: git pull to update the repository
+    } else {
+      // Pull the official repository
+      log.Warn("Directory " + repositoryDirectory + " doesn't exist")
+      command := "git clone https://github.com/runscripts/script.git " + repositoryDirectory
+      output := utils.ExecCommandReturnOutput(command)
+      log.Info(output)
+    }
+
+    if (utils.IsFileExist(scriptPath)) {
+      // Find the script, run it
+      commandArgs := []string{scriptPath}
+      utils.SyscallCommandNotReturn(commandArgs)
+    } else {
+      // The script doesn't exist
+      log.Error("The script " + scriptName + " doesn't exist in official repository")
+      os.Exit(0)
+    }
+
   } else {
-    log.Error("It's fucking mine or yours")
+
+    // It' a user script
+    userName := scriptArray[0]
+    repositoryDirectory := runscriptDirectory + "/" + userName
+    scriptName := scriptArray[1]
+    scriptPath := repositoryDirectory + "/" + scriptName
+
+    if (utils.IsDirectoryExist(repositoryDirectory)) {
+
+    } else {
+      // Mkdir the directory and pull the repository
+      utils.MkdirIfNotExist(repositoryDirectory)
+      log.Warn("Directory " + repositoryDirectory + " doesn't exist")
+      command := "git clone https://github.com/" + userName + "/script.git " + repositoryDirectory
+      output := utils.ExecCommandReturnOutput(command)
+      log.Info(output)
+    }
+
+    if (utils.IsFileExist(scriptPath)) {
+      // Find the script, run it
+      commandArgs := []string{scriptPath}
+      utils.SyscallCommandNotReturn(commandArgs)
+    } else {
+      // The script doesn't exist
+      log.Error("The script " + scriptName + " doesn't exist in " + userName + "'s repository")
+      os.Exit(0)
+    }
   }
 
-  command := "ls"
-  output := utils.ExecCommandReturnOutput(command)
-  log.Info(output)
-
-  //args := []string{"git", "clone", "git@github.com:runscripts/script.git", "/Users/tobe/Desktop/temp/script"}
-  commandArgs := []string{"echo", "hello"}
-  utils.SyscallCommandNotReturn(commandArgs)
-
+  // Never reach here because it switches to execute other command
 }
-
 
 // Print out the usage of runscript
 func help() string {
