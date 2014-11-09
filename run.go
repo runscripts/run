be9cc79dd9d611d1a1d94d164c2d9773a3872909
tobe <tobeg3oogle@gmail.com>
2014-11-09 19:44:45 +0800

    Implement to mkdir basic directories

--- run.go
+++ run.go
@@ -2,28 +2,88 @@
 package main
 
 import (
-  //"os"
-  //"flag"
-
-  "github.com/runscripts/runscript/utils"
+  "os"
+  "flag"
+  "strings"
 
   log "github.com/Sirupsen/logrus"
+  "github.com/runscripts/runscript/utils"
 )
 
-func help() {
-
-}
-
-
+// Implement the command of runscript
 func main() {
   log.Info("Start runscript program")
 
-  command := "ls -al"
+  // Parse command-line arguments
+  args := os.Args
+  updateFlag := flag.Bool("u", false, "Force to update the local script")
+  helpFlag := flag.Bool("h", false, "Check out the usage of runscript")
+  //helpFlag := flag.Bool("help", false, "Check out the usage")
+  flag.Parse()
+
+  // Todo: filter the flags in args array
+
+  // Exit if the args is too less or too more
+  argsLength := len(args)
+  if (argsLength < 2 || argsLength > 3) {
+    log.WithFields(log.Fields{
+      "argsLength": argsLength,
+    }).Error("Args length is too less or too more, exit")
+
+    log.Warn(help())
+    os.Exit(0)
+  }
+
+  // Exit if we pass flag "-h"
+  if (*helpFlag) {
+    log.Warn(help())
+    os.Exit(0)
+  }
+
+  // Todo: implement forcing to download scripts
+  log.Warn(*updateFlag)
+
+  // Create $HOME/runscript/ if it doesn't exist
+  runscriptDirectory := os.Getenv("HOME") + "/runscript/"
+  utils.MkdirIfNotExist(runscriptDirectory)
+
+  // Create $HOME/runscript/official/ if it doesn't exist
+  officialDirectory := os.Getenv("HOME") + "/runscript/official/"
+  utils.MkdirIfNotExist(officialDirectory)
+
+  // Check if it's official scripts
+  scriptArray := strings.Split(args[1], "/")
+  if len(scriptArray) == 1 {
+    log.Error("It's official")
+  } else {
+    log.Error("It's fucking mine or yours")
+  }
+
+  command := "ls"
   output := utils.ExecCommandReturnOutput(command)
   log.Info(output)
 
   //args := []string{"git", "clone", "git@github.com:runscripts/script.git", "/Users/tobe/Desktop/temp/script"}
-  args := []string{"echo", "hello"}
-  utils.SyscallCommandNotReturn(args)
+  commandArgs := []string{"echo", "hello"}
+  utils.SyscallCommandNotReturn(commandArgs)
 
+}
+
+
+// Print out the usage of runscript
+func help() string {
+  return `Usage:
+
+    run $script
+
+    Example: run test-network
+
+    run $user/$script
+
+    Example: run tobegit3hub/while-loop
+
+    run -u $script
+
+    Exmaple: run -u test-network
+  `
 }
--- utils/utils.go
+++ utils/utils.go
@@ -9,6 +9,30 @@ import (
   log "github.com/Sirupsen/logrus"
 )
 
+// Mkdir directory if it doesn't exist
+func MkdirIfNotExist(directory string) {
+  if _, err := os.Stat(directory); os.IsNotExist(err) {
+    log.Warn("No direcotry " + directory)
+    // Todo: handle mkdir failure
+    os.Mkdir(directory, 0777)
+    log.Info("Mkdir " + directory)
+  }
+}
+
+// Check if the file exists
+func IsFileExist(file string) bool {
+  if _, err := os.Stat(file); os.IsNotExist(err) {
+    return false
+  } else {
+    return true;
+  }
+}
+
+// Check if the directory exists
+func IsDirectoryExist(directory string) bool {
+  return IsFileExist(directory)
+}
+
 // Use exec library to execute command
 // Will block and return the output when it's finished
 func ExecCommandReturnOutput(command string) string {
