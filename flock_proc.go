e76ba43bdab0101c5cc36e32f8fbf95751855013
wizawu <wizawu@gmail.com>
2014-11-20 01:09:57 +0800

    simplify code

---
+++ flock_proc.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+	"time"
+	"github.com/runscripts/runscripts/utils"
+)
+
+// This program is run by flock_test.go to verify the lock effect.
+func main() {
+	path := "/tmp/flock_test.lock"
+	if err := utils.Flock(path); err != nil {
+		panic(err)
+	}
+	if os.Args[1] == "0" {
+		time.Sleep(time.Second)
+	}
+	if err := utils.Funlock(path); err != nil {
+		panic(err)
+	}
+}
