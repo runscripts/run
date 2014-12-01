da146d2e9b561e7bc76bb062acc4a2ecbfaa9268
wizawu <wizawu@gmail.com>
2014-12-02 01:18:54 +0800

    add test for NewConfigFromString

--- utils/config_test.go
+++ utils/config_test.go
@@ -11,3 +11,22 @@ func TestNewConfig(t *testing.T) {
 		t.Errorf("Parse result is not correct")
 	}
 }
+
+func TestNewConfigFromString(t *testing.T) {
+	str := `
+[sources]
+# ignore
+default: default_url
+  space: space_url  `
+	config, err := NewConfigFromString(str)
+	if err != nil {
+		t.Error(err)
+	}
+	if config.Sources["default"] != "default_url" {
+		t.Errorf("'default' source is not parsed correctly")
+	} else if config.Sources["space"] != "space_url" {
+		t.Errorf("'space' source is not parsed correctly")
+	} else if _, ok := config.Sources["ignore"]; ok {
+		t.Errorf("'ignore' source should be ignored")
+	}
+}
